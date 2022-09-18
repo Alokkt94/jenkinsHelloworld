862668011048c0164e62c2e5bbebf0f428e4030e
Alokkt94 <56097860+Alokkt94@users.noreply.github.com>
2022-09-18 14:54:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("This is hello world from jenkins");
+		System.out.println("---------------------------");
+		System.out.println("New change for jenkins");
 	}
 }
